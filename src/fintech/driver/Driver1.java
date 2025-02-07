caafefcd867e2d04bffbc87329be3c2d087c4055
placidiasitorus <santasitorus105@gmail.com>
2025-02-07 16:31:53 +0700

    Update Driver1.java

--- src/fintech/driver/Driver1.java
+++ src/fintech/driver/Driver1.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
 public class Driver1 { 
 
     public static void main(String[] _args) {
-     Scanner scanner = new Scanner (System.in);    
+     Scanner scanner = new Scanner (System.in); 
+        String command = scanner.nextLine();
         String owner = scanner.nextLine();
         String accountName = scanner.nextLine();
 
