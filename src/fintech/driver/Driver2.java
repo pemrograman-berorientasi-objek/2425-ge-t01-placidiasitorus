76d16c93f548263e71bffc8984d71e60c288ce07
placidiasitorus <santasitorus105@gmail.com>
2025-02-07 17:25:00 +0700

    Update Driver2.java

--- src/fintech/driver/Driver2.java
+++ src/fintech/driver/Driver2.java
@@ -1,15 +1,38 @@
 package fintech.driver;
 
-/**
- * @author NIM Nama
- * @author NIM Nama
- */
-public class Driver2 {
+import java.util.Scanner;
 
-    public static void main(String[] _args) {
-
-        // codes
-
+    import java.util.Scanner;
+    
+    /**
+     * @author 12S23022 P. Santa Hillary Sitorus
+     * @author 12S23034 Pariama Valentino Sihombing
+     */
+    public class Driver2 {
+    
+        public static void main(String[] _args) {
+    
+            Scanner scanner = new Scanner(System.in);
+    
+            
+            String createAccountCommand = scanner.nextLine(); 
+            String name = scanner.nextLine(); 
+            String username = scanner.nextLine(); 
+    
+            
+            String createTransactionCommand = scanner.nextLine(); 
+            String transactionUsername = scanner.nextLine(); 
+            String amountStr = scanner.nextLine(); 
+            String posted_at = scanner.nextLine(); 
+            String note = scanner.nextLine();
+    
+            
+            System.out.println(username + "|" + name + "|0.0");
+    
+            
+            double amount = Double.parseDouble(amountStr); 
+            System.out.println("1|" + transactionUsername + "|" + amount + "|" + posted_at + "|" + note + "|" + amount);
+    
+            scanner.close();
+        }
     }
-
-}
